d2118997eb898bc7160ecf58113e49b7782822ac
LPTrungGTN <135190672+LPTrungGTN@users.noreply.github.com>
2024-06-17 13:06:07 +0700

    chore: Update layout styles for header and footer components

--- next/app/layouts/footer.tsx
+++ next/app/layouts/footer.tsx
@@ -2,10 +2,7 @@ import type { NextPage } from "next";
 
 const Footer: NextPage = () => {
   return (
-    <footer
-      className="fixed bottom-0 left-0 w-full bg-slate-50  shadow text-center p-5 "
-      style={{ height: "7vh" }}
-    >
+    <footer className="fixed bottom-0 left-0 w-full bg-slate-50  shadow text-center p-5 h-20">
       <a href="#">GTN Vietnam (c) 2023</a>
     </footer>
   );
