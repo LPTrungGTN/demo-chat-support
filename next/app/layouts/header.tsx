fe4f6ee4d9fb11a64a84ef97d7dcfdbb168495ec
LPTrungGTN <135190672+LPTrungGTN@users.noreply.github.com>
2024-06-19 08:24:05 +0700

    refactor: Update header component to use single quotes for import statements and class names

--- next/app/layouts/header.tsx
+++ next/app/layouts/header.tsx
@@ -1,57 +1,45 @@
-"use client";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Cookies from "js-cookie";
-import React, { useContext } from "react";
+'use client';
+import Cookies from 'js-cookie';
+import React from 'react';
 
-import DropDownButton from "@/app/components/dropdownBtn";
-import { AppContext } from "@/app/contexts/AppContext";
+import DropDownButton from '@/app/components/dropdownBtn';
 type IHeader = {
   hasAccessToken: string;
   setHasAccessToken: React.Dispatch<React.SetStateAction<string>>;
 };
 const Header = ({ hasAccessToken, setHasAccessToken }: IHeader) => {
   React.useEffect(() => {
-    const accessToken = Cookies.get("accessToken") || "";
+    const accessToken = Cookies.get('accessToken') || '';
     setHasAccessToken(accessToken);
   }, []);
 
-  const { isSidebarOpen, setIsSidebarOpen } = useContext(AppContext);
   return (
     <>
       <header
-        className="h-20 w-full flex items-center bg-gray-50 "
-        style={{ position: "fixed", zIndex: 20 }}
+        className='h-20 w-full flex items-center bg-gray-50 '
+        style={{ position: 'fixed', zIndex: 20 }}
       >
         {!hasAccessToken && (
-          <div className="flex ">
-            <img src="/logo.png" alt="Logo" className="pl-12" />
+          <div className='flex '>
+            <img src='/logo.png' alt='Logo' className='pl-12' />
 
-            <div className=" text-black text-lg font-bold pl-6 py-auto">
+            <div className=' text-black text-lg font-bold pl-6 py-auto'>
               Attendance
             </div>
           </div>
         )}
 
-        <div className=" w-full p-0 mx-2 lg:mx-12">
+        <div className=' w-full p-0 mx-2 lg:mx-12'>
           {hasAccessToken && (
-            <div className="w-full flex justify-between">
-              <div className="lg:flex w-2/5 hidden ">
-                <img src="/logo.png" alt="Logo" />
+            <div className='w-full flex justify-between'>
+              <div className='lg:flex w-2/5 hidden '>
+                <img src='/logo.png' alt='Logo' />
 
-                <div className=" text-black text-lg ml-8">
+                <div className=' text-black text-lg ml-8'>
                   Attendance System
                 </div>
               </div>
-              <div
-                className="lg:hidden text-3xl cursor-pointer"
-                onClick={() => {
-                  setIsSidebarOpen(!isSidebarOpen);
-                }}
-              >
-                <FontAwesomeIcon icon={faBars} />
-              </div>
-              <img src="/logo.png" alt="Logo" className="p-0 block lg:hidden" />
+              <img src='/logo.png' alt='Logo' className='p-0 block lg:hidden' />
 
               <DropDownButton />
             </div>
