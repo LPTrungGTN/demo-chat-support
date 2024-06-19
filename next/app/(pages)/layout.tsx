77a9507d2b4a4221fd75718860578ee1390bdf88
LPTrungGTN <135190672+LPTrungGTN@users.noreply.github.com>
2024-06-19 08:23:22 +0700

    refactor: Update layout.tsx to use single quotes for import statements and class names

--- next/app/(pages)/layout.tsx
+++ next/app/(pages)/layout.tsx
@@ -1,35 +1,33 @@
-"use client";
-import "react-toastify/dist/ReactToastify.css";
+'use client';
+import 'react-toastify/dist/ReactToastify.css';
 
-import { useState } from "react";
-import { ToastContainer } from "react-toastify";
+import { useState } from 'react';
+import { ToastContainer } from 'react-toastify';
 
-import { AppProvider } from "@/app/contexts/AppContext";
-import Footer from "@/app/layouts/footer";
-import Header from "@/app/layouts/header";
+import { AppProvider } from '@/app/contexts/AppContext';
+import Header from '@/app/layouts/header';
 export default function PagesLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [hasAccessToken, setHasAccessToken] = useState<string>("");
+  const [hasAccessToken, setHasAccessToken] = useState<string>('');
   return (
     <AppProvider>
       <Header
         hasAccessToken={hasAccessToken}
         setHasAccessToken={setHasAccessToken}
       />
-      <div className=" w-full" style={{ height: "85vh" }}>
+      <div className=' w-full' style={{ height: '85vh' }}>
         {hasAccessToken ? (
-          <div className="pt-20 pb-20 h-screen w-full flex antialiased text-gray-200 bg-gray-900 overflow-hidden">
+          <div className='pt-20 h-screen w-full flex antialiased text-gray-200 bg-gray-900 overflow-hidden'>
             {children}
           </div>
         ) : (
-          <div className="col-span-12">{children}</div>
+          <div className='col-span-12'>{children}</div>
         )}
-        <ToastContainer position="top-center" />
+        <ToastContainer position='top-center' />
       </div>
-      <Footer />
     </AppProvider>
   );
 }
