cc7ac9709f87b4cbf516daa35ccb456c20613b16
LPTrungGTN <135190672+LPTrungGTN@users.noreply.github.com>
2024-06-19 12:18:41 +0700

    refactor: Remove migration files for altering "staffStatus" table and adding "status" column

--- nest/prisma/migrations/20240619050631_init/migration.sql
+++ nest/prisma/migrations/20240619050631_init/migration.sql
@@ -1,6 +1,6 @@
 -- CreateTable
 CREATE TABLE "staffs" (
-    "id" INTEGER NOT NULL,
+    "id" SERIAL NOT NULL,
     "username" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -13,6 +13,7 @@ CREATE TABLE "staffStatus" (
     "staff_id" INTEGER NOT NULL,
     "max_active_chats" INTEGER NOT NULL,
     "current_active_chats" INTEGER NOT NULL,
+    "status" INTEGER NOT NULL DEFAULT 0,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "staffStatus_pkey" PRIMARY KEY ("staff_id")
@@ -20,7 +21,7 @@ CREATE TABLE "staffStatus" (
 
 -- CreateTable
 CREATE TABLE "categories" (
-    "id" INTEGER NOT NULL,
+    "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "description" TEXT NOT NULL,
 
@@ -37,12 +38,12 @@ CREATE TABLE "staffCategory" (
 
 -- CreateTable
 CREATE TABLE "chatRooms" (
-    "id" INTEGER NOT NULL,
-    "staff_id" INTEGER NOT NULL,
+    "id" SERIAL NOT NULL,
+    "staff_id" INTEGER,
     "category_id" INTEGER NOT NULL,
     "language" TEXT NOT NULL,
-    "status" TEXT NOT NULL,
-    "rating" INTEGER NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'open',
+    "rating" INTEGER,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "chatRooms_pkey" PRIMARY KEY ("id")
@@ -50,7 +51,7 @@ CREATE TABLE "chatRooms" (
 
 -- CreateTable
 CREATE TABLE "messages" (
-    "id" INTEGER NOT NULL,
+    "id" SERIAL NOT NULL,
     "content" TEXT NOT NULL,
     "staff_id" INTEGER NOT NULL,
     "chat_room_id" INTEGER NOT NULL,
@@ -58,6 +59,9 @@ CREATE TABLE "messages" (
 
     CONSTRAINT "messages_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "staffs_username_key" ON "staffs"("username");
 
 -- AddForeignKey
 ALTER TABLE "staffStatus" ADD CONSTRAINT "staffStatus_staff_id_fkey" FOREIGN KEY ("staff_id") REFERENCES "staffs"("id") ON DELETE CASCADE ON UPDATE CASCADE;
@@ -69,7 +73,7 @@ ALTER TABLE "staffCategory" ADD CONSTRAINT "staffCategory_staff_id_fkey" FOREIGN
 ALTER TABLE "staffCategory" ADD CONSTRAINT "staffCategory_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "categories"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "chatRooms" ADD CONSTRAINT "chatRooms_staff_id_fkey" FOREIGN KEY ("staff_id") REFERENCES "staffs"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "chatRooms" ADD CONSTRAINT "chatRooms_staff_id_fkey" FOREIGN KEY ("staff_id") REFERENCES "staffs"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "chatRooms" ADD CONSTRAINT "chatRooms_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "categories"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
