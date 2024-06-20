50d93746c494fdd6c64662d30777700a0a54a9bc
LPTrungGTN <135190672+LPTrungGTN@users.noreply.github.com>
2024-06-20 16:44:04 +0700

    refactor: Update user page to use ChatBox component for real-time chat functionality

--- next/app/(pages)/user/page.tsx
+++ next/app/(pages)/user/page.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
 
-import ChatBody from '@/app/components/chatBody';
-import ChatFooter from '@/app/components/chatFooter';
-import ChatHeader from '@/app/components/chatHeader';
+import ChatBox from '@/app/components/chatBox';
 
 const LoginPage = () => {
   return (
     <div className='flex-1 flex flex-col'>
       <main className='flex-grow flex flex-row min-h-0'>
-        <section className='flex flex-col flex-auto border-l border-gray-800'>
-          <ChatHeader />
-          <ChatBody />
-          <ChatFooter />
-        </section>
+        <ChatBox />
       </main>
     </div>
   );
