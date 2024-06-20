3ed6d85a7efeba18700ef987e279cd52f8778c16
LPTrungGTN <135190672+LPTrungGTN@users.noreply.github.com>
2024-06-20 08:41:19 +0700

    refactor: Update chatBody.tsx to use single quotes for imports and fix accessToken type

--- next/app/components/chatBody.tsx
+++ next/app/components/chatBody.tsx
@@ -1,25 +1,27 @@
-"use client";
-import "react-toastify/dist/ReactToastify.css";
+'use client';
+import 'react-toastify/dist/ReactToastify.css';
 
-import Cookies from "js-cookie";
-import { useEffect, useState } from "react";
+import Cookies from 'js-cookie';
+import { useEffect, useState } from 'react';
 
-import { messages } from "@/app/(pages)/user/fakeData";
-import MyMessageComponent from "@/app/components/myMessageComponent";
-import OtherMessageComponent from "@/app/components/otherMessageComponent";
+import { messages } from '@/app/(pages)/user/fakeData';
+import MyMessageComponent from '@/app/components/myMessageComponent';
+import OtherMessageComponent from '@/app/components/otherMessageComponent';
 
 const ChatBody = () => {
-  const [accessToken, setAccessToken] = useState(0);
+  const [accessToken, setAccessToken] = useState<string | number>('');
 
   useEffect(() => {
-    const accessToken = Cookies.get("accessToken");
+    const accessToken = Cookies.get('accessToken');
     if (accessToken) {
-      setAccessToken(Number(accessToken));
+      setAccessToken(
+        accessToken === 'customer' ? accessToken : Number(accessToken),
+      );
     }
   }, []);
 
   return (
-    <div className="chat-body p-4 flex-1 overflow-y-scroll">
+    <div className='chat-body p-4 flex-1 overflow-y-scroll'>
       {messages.map((msg) =>
         msg.userId === accessToken ? (
           <MyMessageComponent msg={msg.content} />
