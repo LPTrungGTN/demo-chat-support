e24b329d901c2f288d3b382f4c481408ef533f93
LPTrungGTN <135190672+LPTrungGTN@users.noreply.github.com>
2024-06-20 13:20:29 +0700

    feat: Create useSocket hook for socket.io client connection

---
+++ next/app/utils/hooks/useSocket.ts
@@ -0,0 +1,8 @@
+import { io } from 'socket.io-client';
+
+function createSocket(namespace: string) {
+  const url = process.env.NEXT_PUBLIC_SOCKET_URL || 'http://localhost:3001';
+  return io(`${url}/${namespace}`);
+}
+
+export default createSocket;
