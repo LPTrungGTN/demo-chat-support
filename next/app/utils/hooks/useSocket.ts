28af9b96c9e5a50ab7495d528067724d09ccf067
LPTrungGTN <135190672+LPTrungGTN@users.noreply.github.com>
2024-06-20 16:44:10 +0700

    refactor: Update useSocket.ts to export ChatFooterProps interface

--- next/app/utils/hooks/useSocket.ts
+++ next/app/utils/hooks/useSocket.ts
@@ -1,8 +1,12 @@
-import { io } from 'socket.io-client';
+import { io, Socket } from 'socket.io-client';
 
 function createSocket(namespace: string) {
   const url = process.env.NEXT_PUBLIC_SOCKET_URL || 'http://localhost:3001';
   return io(`${url}/${namespace}`);
+}
+
+export interface ChatFooterProps {
+  socket: Socket;
 }
 
 export default createSocket;
