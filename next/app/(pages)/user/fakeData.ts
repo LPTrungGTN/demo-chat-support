a61e6014135cac7517d47df9f188359cbd897c8f
LPTrungGTN <135190672+LPTrungGTN@users.noreply.github.com>
2024-06-20 08:22:39 +0700

    refactor: Update user fakeData.ts to use single quotes for string literals

--- next/app/(pages)/user/fakeData.ts
+++ next/app/(pages)/user/fakeData.ts
@@ -1,33 +1,33 @@
 export const messages = [
   {
-    content: "start",
+    content: 'start',
     groupId: 2,
-    timeStamp: "2021-10-10T10:00:00",
-    userId: 2,
+    timeStamp: '2021-10-10T10:00:00',
+    userId: 'customer',
   },
   {
     content:
-      "Nice to meet you, the first Can you select the language you want to talk to me?",
+      'Nice to meet you, the first Can you select the language you want to talk to me?',
     groupId: 2,
-    timeStamp: "2021-10-10T10:01:00",
-    userId: 1,
+    timeStamp: '2021-10-10T10:01:00',
+    userId: 5,
   },
   {
-    content: "english",
+    content: 'english',
     groupId: 2,
-    timeStamp: "2021-10-10T10:02:00",
-    userId: 2,
+    timeStamp: '2021-10-10T10:02:00',
+    userId: 'customer',
   },
   {
-    content: "What kind of service do you want to exchange?",
+    content: 'What kind of service do you want to exchange?',
     groupId: 2,
-    timeStamp: "2021-10-10T10:03:00",
-    userId: 1,
+    timeStamp: '2021-10-10T10:03:00',
+    userId: 5,
   },
   {
-    content: "report",
+    content: 'report',
     groupId: 2,
-    timeStamp: "2021-10-10T10:04:00",
-    userId: 2,
+    timeStamp: '2021-10-10T10:04:00',
+    userId: 'customer',
   },
 ];
