9e21a98ea1d0baf75bf79630fea6cdf7662b5ccf
LPTrungGTN <135190672+LPTrungGTN@users.noreply.github.com>
2024-06-20 14:59:12 +0700

    refactor: Update chatBody component to use socket.io for real-time chat functionality

--- next/app/components/chatBody.tsx
+++ next/app/components/chatBody.tsx
@@ -4,12 +4,40 @@ import 'react-toastify/dist/ReactToastify.css';
 import Cookies from 'js-cookie';
 import { useEffect, useState } from 'react';
 
-import { messages } from '@/app/(pages)/user/fakeData';
 import MyMessageComponent from '@/app/components/myMessageComponent';
 import OtherMessageComponent from '@/app/components/otherMessageComponent';
+import createSocket from '@/app/utils/hooks/useSocket';
 
 const ChatBody = () => {
+  const socket = createSocket('chat');
+
   const [accessToken, setAccessToken] = useState<string | number>('');
+  const [roomId, setRoomId] = useState<string>('');
+  const [messages, setMessages] = useState<
+    { message: string; sender: string; timestamp: string; userId: string }[]
+  >([]);
+
+  useEffect(() => {
+    if (socket) {
+      socket.on('newMessage', (data) => {
+        setMessages((prev) => [...prev, data]);
+      });
+
+      socket.on('roomCreated', (data) => {
+        const { id: roomId } = data;
+        setRoomId(roomId);
+        Cookies.set('roomId', roomId);
+      });
+
+      socket.on('staffJoined', (data) => {
+        console.log('Staff joined:', data);
+      });
+
+      socket.on('error', (data) => {
+        console.error(data.message);
+      });
+    }
+  }, [socket]);
 
   useEffect(() => {
     const accessToken = Cookies.get('accessToken');
@@ -18,17 +46,35 @@ const ChatBody = () => {
         accessToken === 'customer' ? accessToken : Number(accessToken),
       );
     }
+
+    const roomIdCookies = Cookies.get('roomId');
+    if (roomIdCookies) {
+      setRoomId(roomIdCookies);
+    }
+
+    if (accessToken === 'customer' && !roomId) {
+      handleCreateRoom();
+    } else if (accessToken !== 'customer') {
+      socket!.emit('joinRoom', { roomId: 5, staffId: 'customer' });
+    } else {
+      socket!.emit('joinRoom', { roomId, staffId: accessToken });
+    }
   }, []);
+
+  const handleCreateRoom = () => {
+    socket.emit('createRoom', { categoryId: 6, language: 'en' });
+  };
 
   return (
     <div className='p-4 flex-1 overflow-y-scroll'>
-      {messages.map((msg) =>
-        msg.userId === accessToken ? (
-          <MyMessageComponent msg={msg.content} />
+      {messages.map((msg) => {
+        const { message, userId } = msg;
+        return userId === accessToken ? (
+          <MyMessageComponent msg={message} />
         ) : (
-          <OtherMessageComponent msg={msg.content} />
-        ),
-      )}
+          <OtherMessageComponent msg={message} />
+        );
+      })}
     </div>
   );
 };
