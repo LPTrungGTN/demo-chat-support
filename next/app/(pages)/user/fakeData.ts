9623268e0cbc32b024c0bd697c1edcf167ce7696
LPTrungGTN <135190672+LPTrungGTN@users.noreply.github.com>
2024-06-20 13:20:18 +0700

    refactor: Update fakeData.ts to use 'message' property instead of 'content'

--- next/app/(pages)/user/fakeData.ts
+++ next/app/(pages)/user/fakeData.ts
@@ -1,32 +1,32 @@
 export const messages = [
   {
-    content: 'start',
     groupId: 2,
+    message: 'start',
     timeStamp: '2021-10-10T10:00:00',
     userId: 'customer',
   },
   {
-    content:
-      'Nice to meet you, the first Can you select the language you want to talk to me?',
     groupId: 2,
+    message:
+      'Nice to meet you, the first Can you select the language you want to talk to me?',
     timeStamp: '2021-10-10T10:01:00',
     userId: 5,
   },
   {
-    content: 'english',
     groupId: 2,
+    message: 'english',
     timeStamp: '2021-10-10T10:02:00',
     userId: 'customer',
   },
   {
-    content: 'What kind of service do you want to exchange?',
     groupId: 2,
+    message: 'What kind of service do you want to exchange?',
     timeStamp: '2021-10-10T10:03:00',
     userId: 5,
   },
   {
-    content: 'report',
     groupId: 2,
+    message: 'report',
     timeStamp: '2021-10-10T10:04:00',
     userId: 'customer',
   },
