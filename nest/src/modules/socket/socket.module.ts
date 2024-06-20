4ffe4d58b09aac34783e0286efb5bf569a8eca25
LPTrungGTN <135190672+LPTrungGTN@users.noreply.github.com>
2024-06-20 14:59:26 +0700

    refactor: Add StaffRepository to providers in SocketModule

--- nest/src/modules/socket/socket.module.ts
+++ nest/src/modules/socket/socket.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 
 import { ChatRoomRepository } from '@/modules/chat-room/chat-room.repository';
+import { StaffRepository } from '@/modules/staff/staff.repository';
 
 import { AppGateway } from './app.gateway';
 
 @Module({
   exports: [AppGateway],
-  providers: [AppGateway, ChatRoomRepository],
+  providers: [AppGateway, ChatRoomRepository, StaffRepository],
 })
 export class SocketModule {}
