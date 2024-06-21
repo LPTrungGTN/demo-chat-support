89a609f9fee02e91815f4b9bd77cfaca57033254
LPTrungGTN <135190672+LPTrungGTN@users.noreply.github.com>
2024-06-21 14:55:13 +0700

    refactor: Add messages to fakeData.ts for testing chat functionality

--- next/app/(pages)/admin/fakeData.ts
+++ next/app/(pages)/admin/fakeData.ts
@@ -25,3 +25,37 @@ export const accounts = [
     time: '31 Dec',
   },
 ];
+
+export const messages = [
+  {
+    groupId: 2,
+    message: 'start',
+    timeStamp: '2021-10-10T10:00:00',
+    userId: 'customer',
+  },
+  {
+    groupId: 2,
+    message:
+      'Nice to meet you, the first Can you select the language you want to talk to me?',
+    timeStamp: '2021-10-10T10:01:00',
+    userId: 5,
+  },
+  {
+    groupId: 2,
+    message: 'english',
+    timeStamp: '2021-10-10T10:02:00',
+    userId: 'customer',
+  },
+  {
+    groupId: 2,
+    message: 'What kind of service do you want to exchange?',
+    timeStamp: '2021-10-10T10:03:00',
+    userId: 5,
+  },
+  {
+    groupId: 2,
+    message: 'report',
+    timeStamp: '2021-10-10T10:04:00',
+    userId: 'customer',
+  },
+];
