ddf202eefb536d727e6e8b11b0861256168a720a
LPTrungGTN <135190672+LPTrungGTN@users.noreply.github.com>
2024-06-21 11:02:50 +0700

    refactor: Add StaffStatusRepository to SocketModule providers

--- nest/src/modules/socket/socket.module.ts
+++ nest/src/modules/socket/socket.module.ts
@@ -2,11 +2,17 @@ import { Module } from '@nestjs/common';
 
 import { ChatRoomRepository } from '@/modules/chat-room/chat-room.repository';
 import { StaffRepository } from '@/modules/staff/staff.repository';
+import { StaffStatusRepository } from '@/modules/staff-status/staff-status.repository';
 
 import { AppGateway } from './app.gateway';
 
 @Module({
   exports: [AppGateway],
-  providers: [AppGateway, ChatRoomRepository, StaffRepository],
+  providers: [
+    AppGateway,
+    ChatRoomRepository,
+    StaffRepository,
+    StaffStatusRepository,
+  ],
 })
 export class SocketModule {}
