735d80547dc58a4b619af38fc7b2b98accce0ad1
LPTrungGTN <135190672+LPTrungGTN@users.noreply.github.com>
2024-06-24 16:40:12 +0700

    refactor: Update ListContact component to use ContactInterface and lazy load contacts

--- next/app/components/listContact.tsx
+++ next/app/components/listContact.tsx
@@ -6,33 +6,36 @@ import { useEffect, useState } from 'react';
 
 import { listByRoomId } from '@/app/api/message';
 import { useChatContext } from '@/app/contexts/chatContext';
+import { RoleEnum } from '@/app/utils/Enums/RoleEnum';
 import { SocketProps } from '@/app/utils/hooks/useSocket';
 
 import Contact from './contact';
 
 const ListContact = ({ socket }: SocketProps) => {
-  const { setMessages } = useChatContext();
+  const { setMessages, setRoomId } = useChatContext();
   const [contacts, setContacts] = useState<ContactInterface[]>([]);
+  const [accessToken, setAccessToken] = useState<string>('');
 
   useEffect(() => {
     if (socket) {
       socket.on('newCustomer', (data) => {
-        setContacts((prev) => [...prev, data]);
+        setContacts((prev) => [data, ...prev]);
       });
 
-      socket.on('staffJoined', (data) => {
-        console.log('Staff joined:', data);
+      socket.on('roomCreated', (data) => {
+        setContacts((prev) => [data, ...prev]);
+        handleContactClick(data.roomId);
       });
 
       socket.on('error', (data) => {
         console.error(data.message);
       });
     }
-  }, [socket]);
+  }, []);
 
   useEffect(() => {
     const getContacts = async () => {
-      const accessToken = Cookies.get('accessToken');
+      setAccessToken(Cookies.get('accessToken')!);
       const data = await listByStaffId(accessToken);
       setContacts(data.rooms);
     };
@@ -44,11 +47,18 @@ const ListContact = ({ socket }: SocketProps) => {
   const handleContactClick = async (roomId: string) => {
     try {
       const data = await listByRoomId(roomId);
+      setRoomId(roomId);
       setMessages(data.messages);
+      socket.emit('joinRoom', { roomId, staffId: accessToken });
     } catch (error) {
       console.error(error);
     }
   };
+
+  const handleCreateRoom = async () => {
+    socket.emit('createRoom', { categoryId: 6, language: 'en' });
+  };
+
   return (
     <section className='flex flex-col flex-none overflow-auto w-24 group lg:max-w-sm md:w-2/5 transition-all duration-300 ease-in-out'>
       <div className='header p-4 flex flex-row justify-between items-center flex-none'>
@@ -65,14 +75,18 @@ const ListContact = ({ socket }: SocketProps) => {
         <p className='text-md font-bold hidden md:block group-hover:block'>
           Messenger
         </p>
-        <a
-          href='#'
-          className='block rounded-full hover:bg-gray-700 bg-gray-800 w-10 h-10 p-2 hidden md:block group-hover:block'
-        >
-          <svg viewBox='0 0 24 24' className='w-full h-full fill-current'>
-            <path d='M6.3 12.3l10-10a1 1 0 0 1 1.4 0l4 4a1 1 0 0 1 0 1.4l-10 10a1 1 0 0 1-.7.3H7a1 1 0 0 1-1-1v-4a1 1 0 0 1 .3-.7zM8 16h2.59l9-9L17 4.41l-9 9V16zm10-2a1 1 0 0 1 2 0v6a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6c0-1.1.9-2 2-2h6a1 1 0 0 1 0 2H4v14h14v-6z' />
-          </svg>
-        </a>
+        {accessToken === RoleEnum.USER ? (
+          <button
+            onClick={handleCreateRoom}
+            className='block rounded-full hover:bg-gray-700 bg-gray-800 w-10 h-10 p-2 hidden md:block group-hover:block'
+          >
+            <svg viewBox='0 0 24 24' className='w-full h-full fill-current'>
+              <path d='M6.3 12.3l10-10a1 1 0 0 1 1.4 0l4 4a1 1 0 0 1 0 1.4l-10 10a1 1 0 0 1-.7.3H7a1 1 0 0 1-1-1v-4a1 1 0 0 1 .3-.7zM8 16h2.59l9-9L17 4.41l-9 9V16zm10-2a1 1 0 0 1 2 0v6a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6c0-1.1.9-2 2-2h6a1 1 0 0 1 0 2H4v14h14v-6z' />
+            </svg>
+          </button>
+        ) : (
+          ''
+        )}
       </div>
       <div className='contacts p-2 flex-1 overflow-y-scroll'>
         {contacts.map((contact) => (
