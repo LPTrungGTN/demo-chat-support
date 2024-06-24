7c59e3313e65050f009e1de79e7ea0bdfadc7269
LPTrungGTN <135190672+LPTrungGTN@users.noreply.github.com>
2024-06-24 09:49:13 +0700

    refactor: Add validation for room ID in MessageController

--- nest/src/modules/message/message.controller.ts
+++ nest/src/modules/message/message.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get, HttpStatus, Param, Res } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  HttpStatus,
+  Param,
+  Res,
+} from '@nestjs/common';
 import { Response } from 'express';
 
 import { MessageService } from './message.service';
@@ -12,7 +19,10 @@ export class MessageController {
     @Param('roomId') roomId: string,
     @Res() res: Response,
   ): Promise<Response> {
-    const messages = await this.service.listMsgByRoomId(Number(roomId));
+    const numericRoomId = Number(roomId);
+    if (isNaN(numericRoomId)) throw new BadRequestException();
+
+    const messages = await this.service.listMsgByRoomId(numericRoomId);
     return res.status(HttpStatus.OK).json({ messages });
   }
 }
