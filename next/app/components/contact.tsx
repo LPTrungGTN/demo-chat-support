01228ff14abb760a0ea7e35c56f0b5022632d206
LPTrungGTN <135190672+LPTrungGTN@users.noreply.github.com>
2024-06-24 13:37:17 +0700

    refactor: Update Contact component to use ContactInterface

--- next/app/components/contact.tsx
+++ next/app/components/contact.tsx
@@ -1,38 +1,28 @@
 'use client';
 import 'react-toastify/dist/ReactToastify.css';
 
+import { ContactInterface } from '../api/chatRoom';
+
 interface Props {
-  contact: {
-    message: string;
-    roomId: string;
-    status: boolean;
-    timestamp: string;
-  };
+  contact: ContactInterface;
   onClick: (roomId: string) => void;
 }
 
 const Contact = ({ contact, onClick }: Props) => {
-  const { message, roomId, status, timestamp } = contact;
+  const { createdAt, message, roomId } = contact;
   return (
     <div
       key={roomId}
       className='flex justify-between items-center p-3 hover:bg-gray-800 rounded-lg relative'
       onClick={() => onClick(roomId)}
     >
-      <p className='text-white text-2xl font-bold'>{roomId + 1}</p>
-      <div className='flex-auto min-w-0 ml-4 mr-6 hidden md:block group-hover:block'>
-        <div
-          className={`flex items-center text-sm ${status ? 'font-bold' : 'text-gray-600'}`}
-        >
-          <div className='min-w-0'>
-            <p className='truncate'>{message}</p>
-          </div>
-          <p className='ml-2 whitespace-no-wrap'>{timestamp}</p>
-        </div>
+      <p className='text-white text-2xl font-bold w-1/6'>{roomId}</p>
+      <div className='text-sm font-bold w-2/3'>
+        <p className='truncate text-slate-500'>{message}</p>
       </div>
-      {status && (
-        <div className='bg-blue-700 w-3 h-3 rounded-full flex flex-shrink-0 hidden md:block group-hover:block'></div>
-      )}
+      <p className='ml-2 whitespace-no-wrap text-xl w-1/6 text-slate-500'>
+        {createdAt}
+      </p>
     </div>
   );
 };
