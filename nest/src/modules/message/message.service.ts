526c2c45af76a6986f89e3d53aa5a84ad64de392
LePhuongTrung <135190672+LPTrungGTN@users.noreply.github.com>
2024-06-25 15:15:34 +0700

    refactor: Update MessageService to return Promise<Message[]> in listMsgByRoomId method

--- nest/src/modules/message/message.service.ts
+++ nest/src/modules/message/message.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@nestjs/common';
 
+import { Message } from '@/modules/chat-room/domain/message';
+
 import { MessageRepository } from './message.repository';
 
 @Injectable()
 export class MessageService {
   constructor(private readonly repository: MessageRepository) {}
 
-  public async listMsgByRoomId(roomId: number) {
+  public async listMsgByRoomId(roomId: number): Promise<Message[]> {
     return await this.repository.listByChatRoomId(roomId);
   }
 }
