343de44fc95b2bb7925cfabce8bbcd4199c03e9f
LPTrungGTN <135190672+LPTrungGTN@users.noreply.github.com>
2024-07-01 12:03:11 +0700

    refactor: Add language and category properties to ChatRoom class
    The ChatRoom class now includes the language and category properties, allowing for better organization and management of chat rooms based on language and category.

--- nest/src/modules/chat-room/domain/chat-room.ts
+++ nest/src/modules/chat-room/domain/chat-room.ts
@@ -5,6 +5,8 @@ export class ChatRoom {
     private readonly chatRoomId: number,
     private readonly message: Message,
     private readonly createdAt: string,
+    private readonly language: string,
+    private readonly categoryId: number | null,
   ) {}
 
   public get getChatRoomId(): number {
@@ -17,5 +19,13 @@ export class ChatRoom {
 
   public get getCreateAt(): string {
     return this.createdAt;
+  }
+
+  public get getLanguage(): string {
+    return this.language;
+  }
+
+  public get getCategoryId(): number | null {
+    return this.categoryId;
   }
 }
