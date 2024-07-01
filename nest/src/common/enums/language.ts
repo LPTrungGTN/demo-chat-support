a1367123fd87be8307605bdac3050e3d91751426
LPTrungGTN <135190672+LPTrungGTN@users.noreply.github.com>
2024-07-01 12:03:01 +0700

    refactor: Remove unused code and update Language enum
    The unused CHINESE and KOREAN language constants were removed from the Language enum in order to clean up the codebase and improve maintainability.

--- nest/src/common/enums/language.ts
+++ nest/src/common/enums/language.ts
@@ -1,8 +1,6 @@
 export const Language = {
-  CHINESE: 'zh',
   ENGLISH: 'en',
   JAPANESE: 'ja',
-  KOREAN: 'ko',
   VIETNAMESE: 'vi',
 } as const;
 export type Language = (typeof Language)[keyof typeof Language];
