88f4855d992395b559f3a709cbebd0e4a27dd639
LPTrungGTN <135190672+LPTrungGTN@users.noreply.github.com>
2024-07-02 14:35:47 +0700

    refactor: Update LanguageNames in language.ts to include Chinese language

--- nest/src/common/enums/language.ts
+++ nest/src/common/enums/language.ts
@@ -6,3 +6,18 @@ export const Language = {
   VIETNAMESE: 'vi',
 } as const;
 export type Language = (typeof Language)[keyof typeof Language];
+
+const LanguageNames: { [key in Language]?: string } = {
+  en: 'English',
+  ja: 'Japanese',
+  ko: 'Korean',
+  vi: 'Vietnamese',
+  zh: 'Chinese',
+};
+
+export function getLanguageName(code: string): string | undefined {
+  if (code in LanguageNames) {
+    return LanguageNames[code as Language];
+  }
+  return undefined;
+}
