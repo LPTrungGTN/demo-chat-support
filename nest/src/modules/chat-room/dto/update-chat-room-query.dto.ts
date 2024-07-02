f580bc732f69ea98a10b37c7e0c3ecabd208cf22
LPTrungGTN <135190672+LPTrungGTN@users.noreply.github.com>
2024-07-02 08:25:45 +0700

    refactor: Update UpdateChatRoomQueryDto to use IsNumber validator for categoryId
    This commit modifies the UpdateChatRoomQueryDto file to update the validation for the categoryId property. The IsString validator is replaced with the IsNumber validator to ensure that the categoryId is a valid number. This change improves the data integrity and reliability of the chat room update functionality.
    
    Refactors:
    - Updated UpdateChatRoomQueryDto to use IsNumber validator for categoryId

--- nest/src/modules/chat-room/dto/update-chat-room-query.dto.ts
+++ nest/src/modules/chat-room/dto/update-chat-room-query.dto.ts
@@ -1,9 +1,9 @@
-import { IsString } from 'class-validator';
+import { IsNumber, IsString } from 'class-validator';
 
 export class UpdateChatRoomBodyDto {
   @IsString()
   language: string;
 
-  @IsString()
+  @IsNumber()
   categoryId: number;
 }
