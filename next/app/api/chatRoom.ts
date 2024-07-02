c84444a42330b08490788281d830b2664b3c4ae3
LPTrungGTN <135190672+LPTrungGTN@users.noreply.github.com>
2024-07-02 12:02:23 +0700

    refactor: Update ChatRoom API to include category and language properties

--- next/app/api/chatRoom.ts
+++ next/app/api/chatRoom.ts
@@ -5,8 +5,10 @@ import { Message } from './message';
 const BASE_URL = process.env.BASE_URL;
 
 export interface ContactInterface {
+  categoryId: number;
   chatRoomId: string;
   createdAt: string;
+  language: string;
   message?: Message;
 }
 
@@ -22,4 +24,15 @@ export const listContact = async (
     `${BASE_URL}/chat_rooms?id=${id}&role=${role}`,
   );
   return response.data;
+};
+
+export const updateCategoryAndLanguage = async (
+  id: number,
+  chatRoomId: string,
+  language: string,
+) => {
+  await axios.put(`${BASE_URL}/chat_rooms/${chatRoomId}`, {
+    categoryId: id,
+    language,
+  });
 };
