4a673e850d48a083d391f02ef7570fecdc8063eb
LPTrungGTN <135190672+LPTrungGTN@users.noreply.github.com>
2024-07-02 12:02:49 +0700

    refactor: Update ChatContext to include language and category properties

--- next/app/contexts/chatContext.tsx
+++ next/app/contexts/chatContext.tsx
@@ -3,11 +3,15 @@ import { Message } from '@api/message';
 import { createContext, ReactNode, useContext, useState } from 'react';
 
 interface ChatContextType {
+  categoryId: number | null;
   chatRoomId: string;
   contacts: ContactInterface[];
+  language: string;
   messages: Message[];
+  setCategoryId: React.Dispatch<React.SetStateAction<number | null>>;
   setChatRoomId: (chatRoomId: string) => void;
   setContacts: React.Dispatch<React.SetStateAction<ContactInterface[]>>;
+  setLanguage: React.Dispatch<React.SetStateAction<string>>;
   setMessages: React.Dispatch<React.SetStateAction<Message[]>>;
 }
 
@@ -29,13 +33,19 @@ export const ChatProvider: React.FC<ChatProviderProps> = ({ children }) => {
   const [messages, setMessages] = useState<Message[]>([]);
   const [chatRoomId, setChatRoomId] = useState<string>('');
   const [contacts, setContacts] = useState<ContactInterface[]>([]);
+  const [language, setLanguage] = useState<string>('');
+  const [categoryId, setCategoryId] = useState<number | null>(null);
 
   const value = {
+    categoryId,
     chatRoomId,
     contacts,
+    language,
     messages,
+    setCategoryId,
     setChatRoomId,
     setContacts,
+    setLanguage,
     setMessages,
   };
 
